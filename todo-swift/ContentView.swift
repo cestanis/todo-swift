0b205c901073d74ac6ec544e3d902c3027d3c563
Mehmet Safak <soezesenlik@ucdavis.edu>
2023-09-22 13:52:26 +0200

    Initial Commit

---
+++ todo-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-swift
+//
+//  Created by Mehmet Özesenlik on 9/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-swift/todo_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_swiftApp.swift
+//  todo-swift
+//
+//  Created by Mehmet Özesenlik on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct todo_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
